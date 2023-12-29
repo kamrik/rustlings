3eae454277256776b282588ddf0cda7d5eff9012
Mark Koudritsky <kamrik@gmail.com>
2023-12-28 20:40:07 -0500

    Actually at 51% now! Done quiz 2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -22,6 +21,11 @@ use std::collections::HashMap;
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+impl Team {
+    fn new(scored: u8, condeded: u8) -> Team {
+        Team {goals_scored: scored, goals_conceded: condeded}
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +43,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name)
+            .and_modify(|t| {t.goals_scored += team_1_score; t.goals_conceded += team_2_score;})
+            .or_insert(Team::new(team_1_score, team_2_score));
+
+        scores.entry(team_2_name)
+            .and_modify(|t| {t.goals_scored += team_2_score; t.goals_conceded += team_1_score;})
+            .or_insert(Team::new(team_2_score, team_1_score));
+
+        // let t1 = scores.get(&team_1_name).unwrap_or(&Team{goals_scored:0, goals_conceded:0});
+        // let t2 = scores.get(&team_2_name).unwrap_or(&Team{goals_scored:0, goals_conceded:0});
+        // scores.insert(team_1_name, Team{goals_scored: (t1.goals_scored + team_1_score), goals_conceded: (t1.goals_conceded + team_2_score)});
+        // scores.insert(team_2_name, Team{goals_scored: (t2.goals_scored + team_2_score), goals_conceded: (t2.goals_conceded + team_1_score)});
+
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -28,15 +27,42 @@ pub enum Command {
     Append(usize),
 }
 
+
+
 mod my_module {
     use super::Command;
 
+    fn append(st: &String, n: &usize) -> String {
+        
+        // https://github.com/hoodie/concatenation_benchmarks-rs
+        let mut s = String::with_capacity(st.len() + 3*n); // lengths are in bytes here
+        s.push_str(st);
+        for i in 0..*n {
+            s.push_str("bar");
+        }
+        
+
+        // let mut s = st.to_owned();
+        // for i in 0..*n {
+        //     s += "bar";
+        // }
+        return s
+    }
+
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output:Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let s = {
+                match command {
+                    Command::Uppercase => string.to_uppercase(),
+                    Command::Trim => string.trim().to_owned(),
+                    Command::Append(n) => append(string, n)
+                }
+            };
+            output.push(s)
         }
         output
     }
@@ -45,7 +71,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
